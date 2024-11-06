6a2e72263f60ab2fa7d1350f9f3c128776a122f6
Rony Miguel <150085212+MiguelRony@users.noreply.github.com>
2024-11-06 00:59:41 -0600

    Add random and simple coloring

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+
 const container = document.querySelector("#container");
+let randomColors = true;
 
 function createGrid(m, n) {
     for (let i = 0; i < m; i++) {
@@ -7,12 +9,36 @@ function createGrid(m, n) {
         for (let j = 0; j < n; j++) {
             const columnItem = document.createElement("div")
             columnItem.className = "grid-column"
-            columnItem.addEventListener("mouseover", () => columnItem.style.backgroundColor = "green")
             rowItem.appendChild(columnItem);
         }
         container.appendChild(rowItem)
     }
+
+    setSquaresActions();
+    
 }
+
+function setSquaresActions(){
+    const elementsRow = container.querySelectorAll('*');
+    elementsRow.forEach(elem => {
+        const elements = elem.querySelectorAll('*');
+        elements.forEach(element => {
+            element.addEventListener('mouseover', () => {
+                const computedStyle = window.getComputedStyle(element)
+                const backgroundColor = computedStyle.getPropertyValue('background-color')
+                if(backgroundColor == 'rgba(0, 0, 0, 0)' || element.style.backgroundColor == "white"){
+                    if (randomColors){
+                        element.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`
+                    }else{
+                        element.style.backgroundColor = `green`
+                    }
+                    
+                }
+            })
+        })
+    })
+}
+
 
 const btn = document.querySelector("#btn-grid")
 btn.addEventListener("click", () => {
@@ -27,11 +53,27 @@ btn.addEventListener("click", () => {
 })
 
 const rst = document.querySelector("#reset-grid");
-rst.addEventListener("click", () => {
+rst.addEventListener("click", resetSquares)
+
+function resetSquares() {
     const elements = container.querySelectorAll('*');
     elements.forEach(element => {
         element.style.backgroundColor = "white";
     })
-})
+}
 
+const coloringBtn = document.querySelector("#coloring");
+coloringBtn.addEventListener('click', ()=>{
+    if (coloringBtn.textContent === "Simple coloring"){
+        randomColors = false
+        resetSquares();
+        setSquaresActions();
+        coloringBtn.textContent = "Random coloring"
+    }else{
+        randomColors = true
+        resetSquares();
+        setSquaresActions();
+        coloringBtn.textContent = "Simple coloring"
+    }
+})
 createGrid(16, 16);
